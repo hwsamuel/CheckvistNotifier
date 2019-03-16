0a25e305e6b2abf9b3a5751b73c0df3753507bbe
Hamman Samuel <hwsamuel@ualberta.ca>
2019-03-16 10:51:42 -0400

    Refactor to support multi-lists

--- run.py
+++ run.py
@@ -3,8 +3,6 @@
 import requests, json, time, re, pytz, datetime
 from settings import *
 
-DUE_DEFAULT = '10h'
-
 def get_today_date(time_zone):
 	my_zone = pytz.timezone(time_zone)
 	my_date = datetime.datetime.now(my_zone)
@@ -16,9 +14,18 @@ def get_auth(USER_NAME, API_KEY):
 	response = requests.post(ping_url, post_data)
 	token = response.text.replace('"', '').strip()
 	return token
+
+def get_lists(auth_token):
+	ping_url = 'https://checkvist.com/checklists.json'
+	response = requests.get(ping_url, {'token': auth_token})
+	all_lists = json.loads(response.text)
+	list_ids = []
+	for list in all_lists:
+		list_ids.append(str(list['id']))
+	return list_ids
 	
 def get_tasks(auth_token, list_id):
-	ping_url = 'https://checkvist.com/checklists/'+str(list_id)+'/tasks.json'
+	ping_url = 'https://checkvist.com/checklists/'+list_id+'/tasks.json'
 	response = requests.get(ping_url, {'token': auth_token})
 	all_tasks = json.loads(response.text)
 	return all_tasks
@@ -35,6 +42,7 @@ def get_today_tasks(all_tasks):
 		title = task['content']
 		tags = task['tags_as_text']
 		due = task['due']
+		list_id = str(task['checklist_id'])
 		
 		if status != 0 or due == None or due != today: continue
 		
@@ -42,20 +50,24 @@ def get_today_tasks(all_tasks):
 		due_time = due_time[0] if len(due_time) > 0 else DUE_DEFAULT
 		due_time = due_time.zfill(3)
 		
-		today_tasks.append({'title': title, 'due_time': due_time})
+		today_tasks.append({'title': title, 'due_time': due_time, 'list_id': list_id})
 		
 	return today_tasks
 	
-def send_push(title, message):
+def send_push(title, message, list_id):
 	ping_url = 'https://wirepusher.com/send'
-	data = {'id': DEVICE_ID, 'title': title, 'message': message, 'action': 'https://m.checkvist.com/app/list/'+str(LIST_ID)}
+	data = {'id': DEVICE_ID, 'title': title, 'message': message, 'action': 'https://m.checkvist.com/app/list/'+list_id}
 	response = requests.post(ping_url, data)
 	print '[' + str(response.status_code) + '] ' + title
 
 def main():
 	auth_token = get_auth(USER_NAME, API_KEY)
-	all_tasks = get_tasks(auth_token, LIST_ID)
-	today_tasks = get_today_tasks(all_tasks)
+	all_lists = get_lists(auth_token)
+	today_tasks = []
+	for list_id in all_lists:
+		list_tasks = get_tasks(auth_token, list_id)
+		todays = get_today_tasks(list_tasks)
+		today_tasks.extend(todays)
 
 	on_date = get_today_date(USER_TIMEZONE)
 	current_hour = str(on_date.strftime("%H"))+'h'
@@ -64,9 +76,10 @@ def main():
 	for task in today_tasks:
 		due_time = task['due_time']
 		title = task['title']
+		list_id = task['list_id']
 		
 		if current_hour != due_time: continue
-		send_push(title, title)
+		send_push(title, title, list_id)
 
 if __name__ == "__main__" :
 	main()
